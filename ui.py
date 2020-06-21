2fcda4c38ff2a3cc2fd1098760616a6e8bdf87bd
akash-kandasamy <akashram953@gmail.com>
2020-06-21 21:05:25 +0400

    commit

--- ui.py
+++ ui.py
@@ -1,5 +1,5 @@
 import sys
-
+from PyQt5 import QtCore
 from PyQt5.QtCore import QPoint
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication as App
@@ -8,11 +8,15 @@ from PyQt5.QtWidgets import QVBoxLayout as VertArr
 from PyQt5.QtWidgets import QLabel as Label
 from PyQt5.QtWidgets import QWidget as Widg
 from PyQt5.QtWidgets import QPushButton as Pbutton
+from PyQt5.QtWidgets import QSlider as Slider
 from PyQt5.QtWidgets import QDial as Dial
+
 from PyQt5 import QtGui
 
 Primaryfont = QtGui.QFont('Jetbrains Mono',12,QtGui.QFont.Bold)
-Secondaryfont = QtGui.QFont('Jetbrains Mono',14,QtGui.QFont.Medium)
+Secondaryfont = QtGui.QFont('Jetbrains Mono',10,QtGui.QFont.Medium)
+SliderFont = QtGui.QFont('Jetbrains Mono',90,QtGui.QFont.Bold)
+Workmins = 25
 
 
 
@@ -25,12 +29,10 @@ class MainWindow(Widg):
 
         #layout setting
         self.layout.addWidget(TitleBar(self))
-        self.layout.addWidget(self.dial)
+        self.layout.addWidget(Main(self))
         self.layout.addStretch(0)
         self.layout.setContentsMargins(0,0,0,0)
 
-        #dial setting
-        self.dial.setFixedHeight(300)
         #self setting
         self.setLayout(self.layout)
         self.setMaximumHeight(400)
@@ -38,7 +40,9 @@ class MainWindow(Widg):
         self.setMinimumHeight(400)
         self.setMinimumWidth(400)
         self.setWindowFlag(Qt.FramelessWindowHint)
-        
+
+
+
 
 
 
@@ -46,7 +50,7 @@ class TitleBar(Widg):
     def __init__(self,parent):
         super(TitleBar,self).__init__()
         #component
-        self.label = Label("Hello")
+        self.label = Label("PORMODO")
         self.layout = HoriArr()
         self.closeButton = Pbutton()
     
@@ -70,11 +74,175 @@ class TitleBar(Widg):
                                     border:none;
                                     """)
         self.closeButton.clicked.connect(self.closeButtonClicked)
+        self.closeButton.installEventFilter(self)
         #self setting
         self.setLayout(self.layout)
 
     def closeButtonClicked(self):
         app.exit()
+    
+    def eventFilter(self,obj,event):
+        if obj == self.closeButton and event.type() == QtCore.QEvent.HoverEnter:
+            self.closeButton.setStyleSheet("""
+                                        background-image:url(closeHovered.png);
+                                        height:30px;
+                                        width:30px;
+                                        border:none;
+                                        """)
+        if obj == self.closeButton and event.type() == QtCore.QEvent.HoverLeave:
+                    self.closeButton.setStyleSheet("""
+                                    background-image:url(close.png);
+                                    height:30px;
+                                    width:30px;
+                                    border:none;
+                                    """)
+        return super(TitleBar, self).eventFilter(obj, event)
+
+
+class Main(Widg):
+    def __init__(self,parent):
+        super(Main,self).__init__()
+        #components
+        self.Vlayout = VertArr()
+        #vlayout setting
+        self.Vlayout.addWidget(SubMain(self))
+        self.Vlayout.addWidget(Play(self))
+        self.Vlayout.setAlignment(Qt.AlignCenter)
+        #self setting
+        self.setLayout(self.Vlayout)
+    
+
+class SubMain(Widg):
+    def __init__(self,parent):
+        super(SubMain,self).__init__()
+        #component
+        self.Hlayout = HoriArr()
+        #hlayout setting
+        self.Hlayout.addWidget(WorkSlider(self))
+        self.Hlayout.addWidget(BreakSlider(self))
+        self.Hlayout.setContentsMargins(0,0,0,0)
+        self.Hlayout.setAlignment(Qt.AlignCenter)
+        # self setting
+        self.setLayout(self.Hlayout)
+        
+
+
+class WorkSlider(Widg):
+    mins = 25
+    def __init__(self,parent):
+        super(WorkSlider,self).__init__()
+        #components
+        self.layout = VertArr()
+        self.slider = Slider()
+        self.label = Label('25')
+        #label Setting
+        self.label.setFont(SliderFont)
+        self.label.setAlignment(Qt.AlignCenter)
+        
+        #slider setting
+        self.slider.setRange(0,60)
+        self.slider.setOrientation(Qt.Horizontal)
+        self.slider.setTickPosition(Slider.TicksBelow)
+        self.slider.setTickInterval(5)
+        self.slider.valueChanged.connect(self.sliderValuedChanged)
+        self.slider.setFixedWidth(150)
+        self.slider.setValue(25)
+        
+
+        #self.layout setting
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.slider)
+        self.layout.setAlignment(Qt.AlignCenter)
+
+        #self setting
+        self.setLayout(self.layout)
+    def sliderValuedChanged(self):
+        print(self.slider.value())
+        Workmins = self.slider.value()
+        self.label.setText(str(self.slider.value()))
+        print(Workmins)
+
+
+
+
+class BreakSlider(Widg):
+    mins = 5
+    def __init__(self,parent):
+        super(BreakSlider,self).__init__()
+        #components
+        self.layout = VertArr()
+        self.slider = Slider()
+        self.label = Label('5')
+        
+
+        #label Setting
+        self.label.setFont(SliderFont)
+        self.label.setAlignment(Qt.AlignCenter)
+        
+        #slider setting
+        self.slider.setRange(0,60)
+        self.slider.setOrientation(Qt.Horizontal)
+        self.slider.setTickPosition(Slider.TicksBelow)
+        self.slider.setTickInterval(5)
+        self.slider.valueChanged.connect(self.sliderValuedChanged)
+        self.slider.setFixedWidth(150)
+        self.slider.setValue(5)
+        
+
+        #self.layout setting
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.slider)
+        self.layout.setAlignment(Qt.AlignCenter)
+
+        #self setting
+        self.setLayout(self.layout)
+
+        #self variable 
+
+    def sliderValuedChanged(self):
+        print(self.slider.value())
+        self.label.setText(str(self.slider.value()))
+
+
+    
+
+
+class Play(Widg):
+    def __init__(self,parent):
+        super(Play,self).__init__()
+        #component
+        self.layout = HoriArr()
+        self.label = Label('heh')
+        self.playButton = Pbutton('Start')
+        self.pauseButton = Pbutton('Pause')
+        self.WorkMins = 25
+        self.BreakMins = 25
+
+        #layout setting
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.playButton)
+        self.layout.addWidget(self.pauseButton)
+        
+        #playbutton Setting
+        self.playButton.setFont(Secondaryfont)
+        self.playButton.clicked.connect(self.SetText)
+
+        #pauseButton setting
+        self.pauseButton.setFont(Secondaryfont)
+
+        #self setting
+        self.setLayout(self.layout)
+
+    def SetText(self):
+        self.label.setText(str(Workmins))
+
+
+
+        
+
+
+        
+
 
 
 
