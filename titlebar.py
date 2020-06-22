361b5be1cb1e1142bdc7364ebbd0f7a38a5bac94
akash-kandasamy <akashram953@gmail.com>
2020-06-22 18:51:16 +0400

    commit

--- titlebar.py
+++ titlebar.py
@@ -1,24 +1,17 @@
-from gui import mw
+import sys
+import time
 
-#title Bar component
-mw.TitleBarlabel = Label("PORMODO")
-mw.TitleBarlayout = HoriArr()
-mw.TitleBarcloseButton = Pbutton()
-#TitleBar layout settinh
-mw.TitleBarlayout.addWidget(mw.TitleBarlabel)
-mw.TitleBarlayout.addWidget(mw.TitleBarcloseButton)
-mw.TitleBarlayout.setContentsMargins(0,0,0,0)
-mw.TitleBarlayout.setAlignment(Qt.AlignTop)
-#Title Bar label setting
-mw.TitleBarlabel.setStyleSheet("""background-color:#6DF2A7;""")
-mw.TitleBarlabel.setFixedHeight(30)
-mw.TitleBarlabel.setFixedWidth(400)
-mw.TitleBarlabel.setFont(Primaryfont)
-mw.TitleBarlabel.setAlignment(Qt.AlignCenter)
-#Title bar close button setting
-mw.TitleBarcloseButton.setStyleSheet("""
-                            background-image:url(close.png);
-                            height:30px;
-                            width:30px;
-                            border:none;
-                            """)
+from PyQt5 import QtCore, QtGui
+from PyQt5.QtCore import QPoint, Qt
+from PyQt5.QtWidgets import QApplication as App
+from PyQt5.QtWidgets import QDial as Dial
+from PyQt5.QtWidgets import QHBoxLayout as HoriArr
+from PyQt5.QtWidgets import QLabel as Label
+from PyQt5.QtWidgets import QPushButton as Pbutton
+from PyQt5.QtWidgets import QSlider as Slider
+from PyQt5.QtWidgets import QVBoxLayout as VertArr
+from PyQt5.QtWidgets import QWidget as Widg
+
+Primaryfont = QtGui.QFont('Jetbrains Mono',12,QtGui.QFont.Bold)
+Secondaryfont = QtGui.QFont('Jetbrains Mono',10,QtGui.QFont.Medium)
+SliderFont = QtGui.QFont('Jetbrains Mono',90,QtGui.QFont.Bold)
